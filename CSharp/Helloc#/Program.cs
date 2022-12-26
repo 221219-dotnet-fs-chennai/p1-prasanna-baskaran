fe90e6876416dca9bce634e7eb593a1268fb0d8b
Prasanna-Baskaran <baskarsupra@gmail.com>
2022-12-26 11:53:23 +0530

    sample files

---
+++ CSharp/Helloc#/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("please enter your name");
+String name = Console.ReadLine();
+Console.WriteLine($"Hello, Prasanna-Baskaran! and input is {name}");
